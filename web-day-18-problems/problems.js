1b6b96460ebba417f48e76f13482dd31a10c0d78
najmul-islam <itzmenajmul@gmail.com>
2026-01-04 21:55:30 +0600

    solved bonus problems about array

--- web-day-18-problems/problems.js
+++ web-day-18-problems/problems.js
@@ -146,3 +146,85 @@ if(age < 10){
 }else{
     console.log("Regular ticket fare 800 tk");
 }
+
+
+// BONUS ROUND - ABOUT ARRAY
+
+// js-array-tasks
+
+/*1. Declare an array
+Declare an array with 5 elements containing fruits
+console log the 3rd index element
+change the value of the 2nd index element to jambura
+console log the final array */
+
+const fruits = ['banana', 'mango', 'apple', 'orange', 'litchi'];
+console.log("Third fruits is - ", fruits[2]);
+fruits[2] = 'jambura';
+console.log(fruits);
+
+/*2. Add or remove elements
+Declare an array of 3 tourist destinations
+Add a new tourist destination to your tourist array
+Add two more to your array
+Remove the last tourist destination you have added
+display the final array as output */
+
+const touristDestinations = ['Sylhet', 'CoxBazar', 'BandarBan'];
+touristDestinations.push('Sajek');
+touristDestinations.push('Dhaka', 'Chattagram');
+touristDestinations.pop();
+console.log(touristDestinations);
+
+/*3. Checking Array Membership with ‘includes’
+Instructions:
+
+Create an array of books containing different book.
+
+Use the includes method to check if the array contains a javascript book.
+
+Print a message to the console indicating whether the element is present in the array or not. */
+
+const books = ['himu', 'shubro', 'misirAli'];
+if(books.includes('JavaScript')){
+    console.log("The container have js book.")
+}else{
+    console.log("Js book not exist in this container.")
+};
+
+/*4. Checking if it's an Array
+Instructions:
+
+Create different variables, each containing either an array or a non-array value.
+
+Now use isArray to check if each variable is an array.
+
+Print a message to the console indicating whether each variable is an array or not. */
+
+const var1 = ['hello', 121, true];
+const var2 = 'Hi';
+const var3 = 313;
+const var4 = ['false'];
+
+if(Array.isArray(var4)){
+    console.log('Yes this variable contain array value');
+}else{
+    console.log('No, This is not an array');
+}
+
+
+/*
+5. Combining Arrays
+Instructions:
+
+Create two arrays of your choice.
+Use the concat method to combine the two arrays into a new array.
+Print both the original arrays and the combined array using console.log(). */
+
+const array1 = ['hello', 'hi', 'how are you'];
+const array2 = [120, 20, 80, 70];
+const sumArray = array1.concat(array2);
+
+console.log("First array", array1)
+console.log("Second array", array2)
+console.log("Combine array", sumArray)
